2c954427f425dcf7d4cdc7c10bfb34764bf0cf29
keithko <keith.kolakowski@stichtingheldeninit.nl>
2026-03-23 15:39:43 +0100

    second slider
    trying to put a delay with a second slider . is still in work

--- Assets/Script/UI/HealthBar.cs
+++ Assets/Script/UI/HealthBar.cs
@@ -1,22 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour 
 {
     [SerializeField] private Slider _hPSlider;
+    [SerializeField] private Slider _delaySlider;
+    [SerializeField] private Gradient _gradient;
     [SerializeField] private TextMeshProUGUI _SliderTextHP;
+    [SerializeField] private Image fill;
 
     public void SetSliderMaxValue(int maxhealth)
     {
         _hPSlider.maxValue = maxhealth;
+        fill.color = _gradient.Evaluate(1f);
     }
 
      public void UpdateHealthUI(int currentHealth)
     {
         _SliderTextHP.text = (currentHealth).ToString();
         _hPSlider.value = currentHealth;
+        _delaySlider.value = math.lerp(_delaySlider.value, _hPSlider.value, Time.deltaTime);
+        fill.color = _gradient.Evaluate(_hPSlider.normalizedValue);
     }
 }
