f5d4cd72595977cdfef859c86234adbaa19d6250
MikeHuijgen <mikeh162001@hotmail.com>
2026-03-25 09:18:16 +0100

    You can now only move one tile from there current position

--- Assets/Script/LevelGrid/LevelGrid.cs
+++ Assets/Script/LevelGrid/LevelGrid.cs
@@ -38,6 +38,12 @@ public class LevelGrid : MonoBehaviour
         if (!_gridSystem.IsValidGridPosition(beginGridPosition) || !_gridSystem.IsValidGridPosition(endGridPosition)) return;
         if (beginGridPosition == endGridPosition) return;
 
+        var dx = Mathf.Abs(beginGridPosition.X - endGridPosition.X);
+        var dy = Mathf.Abs(beginGridPosition.Y - endGridPosition.Y);
+
+        if (dx > 1 || dy > 1 || (dx == 0 && dy == 0)) return;
+
+
         var gridObjectA = _gridSystem.GetGridObjectByGridPosition(beginGridPosition);
         var gridObjectB = _gridSystem.GetGridObjectByGridPosition(endGridPosition);
 
