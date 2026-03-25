96a83e14f482dde69e7a8929369eb107c1447bf0
keithko <keith.kolakowski@stichtingheldeninit.nl>
2026-03-25 14:32:43 +0100

    prefab health bar
    health bar has now a basis and have a working delay that waits 0.5 sec before following the health bar

--- Assets/Script/UI/HealthBar.cs
+++ Assets/Script/UI/HealthBar.cs
@@ -12,10 +12,38 @@ public class HealthBar : MonoBehaviour
     [SerializeField] private Gradient _gradient;
     [SerializeField] private TextMeshProUGUI _SliderTextHP;
     [SerializeField] private Image fill;
+    
+    [SerializeField] private float _delaySpeed;
+    
+    [SerializeField] private float _delayTimer;
 
+    private float _currentDelayTime;
+
+    private void Start()
+    {
+        _currentDelayTime = _delayTimer;
+    }
+
+    private void Update()
+    {
+        if (_hPSlider.value == _delaySlider.value)return;
+        _currentDelayTime -= Time.deltaTime;
+        if (_currentDelayTime > 0) return;
+        if (_delaySlider.value > _hPSlider.value)
+        {
+            _delaySlider.value -= Time.deltaTime * _delaySpeed;
+        }
+        else
+        {
+            _delaySlider.value = _hPSlider.value;
+            _currentDelayTime = _delayTimer;
+        }
+    }
     public void SetSliderMaxValue(int maxhealth)
     {
         _hPSlider.maxValue = maxhealth;
+        _delaySlider.maxValue = maxhealth;
+        _delaySlider.value = _hPSlider.value;
         fill.color = _gradient.Evaluate(1f);
     }
 
@@ -23,7 +51,7 @@ public class HealthBar : MonoBehaviour
     {
         _SliderTextHP.text = (currentHealth).ToString();
         _hPSlider.value = currentHealth;
-        _delaySlider.value = math.lerp(_delaySlider.value, _hPSlider.value, Time.deltaTime);
+        _currentDelayTime = _delayTimer; 
         fill.color = _gradient.Evaluate(_hPSlider.normalizedValue);
     }
 }
