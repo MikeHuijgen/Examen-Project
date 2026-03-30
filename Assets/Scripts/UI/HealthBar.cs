1fa40caf40628a90f7cbe81aff4c4d63f1cec1d3
keithko <keith.kolakowski@stichtingheldeninit.nl>
2026-03-30 09:48:20 +0200

    spelling corect
    saw that 1 int was not corect with the code coventions so updated that

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -36,10 +36,10 @@ public class HealthBar : MonoBehaviour
             _currentDelayTime = delayTimer;
         }
     }
-    public void SetSliderMaxValue(int maxhealth)
+    public void SetSliderMaxValue(int maxHealth)
     {
-        healthSlider.maxValue = maxhealth;
-        delaySlider.maxValue = maxhealth;
+        healthSlider.maxValue = maxHealth;
+        delaySlider.maxValue = maxHealth;
         delaySlider.value = healthSlider.value;
         fill.color = gradient.Evaluate(1f);
     }
