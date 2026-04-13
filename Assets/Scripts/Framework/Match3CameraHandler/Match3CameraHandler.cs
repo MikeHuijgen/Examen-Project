fa29a6a1391f3b83afd027b9c273591e8a49da89
MikeHuijgen <mikeh162001@hotmail.com>
2026-04-13 10:04:51 +0200

    Update Match3CameraHandler.cs

--- Assets/Scripts/Framework/Match3CameraHandler/Match3CameraHandler.cs
+++ Assets/Scripts/Framework/Match3CameraHandler/Match3CameraHandler.cs
@@ -6,8 +6,20 @@ public class Match3CameraHandler : MonoBehaviour
 
     private void Start()
     {
+        AdjustTransformPosition();
+        AdjustCamera();
+    }
+
+    private void AdjustTransformPosition()
+    {
         var x = levelGridData.GridWidth * levelGridData.GridCellWidth / 2;
         var y = levelGridData.GridHeight * levelGridData.GridCellHeight / 2;
-        transform.position = new Vector3(x,y, transform.position.z);
+        transform.position = new Vector3(x,y, transform.position.z);        
+    }
+
+    private void AdjustCamera()
+    {
+        float aspectRatio = (float)Screen.width / Screen.height;
+        CameraHolder.Match3Camera.orthographicSize = levelGridData.GridWidth * levelGridData.GridCellWidth / aspectRatio / 3f + 1f;
     }
 }
