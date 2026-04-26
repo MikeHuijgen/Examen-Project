7ac10a75f3402ffae32ace2bdb81a90e45d98ce2
MikeHuijgen <mikeh162001@hotmail.com>
2026-04-26 15:01:28 +0200

    Created the rule flag and moved the blockvisualmanager to its own gameobject

--- Assets/Scripts/Data/Match3BlockProfile/Match3BlockProfile.cs
+++ Assets/Scripts/Data/Match3BlockProfile/Match3BlockProfile.cs
@@ -1,9 +1,8 @@
-using System;
-using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "Match3BlockProfile", menuName = "Scriptable Objects/Match3/Profile")]
 public class Match3BlockProfile : ScriptableObject
 {
     public BaseAttack Attack;
+    public RuleFlag[] ruleFlags;
 }
---
+++ Assets/Scripts/Data/RuleFlag/RuleFlag.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RuleFlag", menuName = "Scriptable Objects/Match3/RuleFlag")]
+public class RuleFlag : ScriptableObject{}
